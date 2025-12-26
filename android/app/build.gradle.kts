9c25608afd75d54bb831e593d92e1413fbd3533b
kiklabautermann <kiklabautermann@gmail.com>
2025-12-26 15:20:15 +0100

    Update Android project structure and fix package naming
    - Updated Android project to use new package name com.example.bimmerdash
    - Fixed Android v2 embedding compatibility
    - Updated build.gradle.kts and AndroidManifest.xml
    - Added test directory structure

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.obd2app"
+    namespace = "com.example.bimmerdash"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.obd2app"
+        applicationId = "com.example.bimmerdash"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/example/bimmerdash/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/bimmerdash/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.obd2app
+package com.example.bimmerdash
 
 import io.flutter.embedding.android.FlutterActivity
 
